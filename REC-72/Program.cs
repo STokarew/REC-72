010342a3ecd07d11d2aa521775e64f04ec763961
сергей токарев <sseregatokarew@gmail.com>
2021-11-18 09:36:58 +0700

    Добавьте файлы проекта.

---
+++ REC-72/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace REC_72
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку целых чисел разделяя из запятой");
+            var ARR = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            Array.Sort(ARR);
+            Console.Write($"Отсортированная последовательность : ");
+            foreach (int i in ARR)
+                Console.Write($"{i} ");
+            Console.ReadKey();
+        }
+    }
+}
